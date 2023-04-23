9220fad571fc40b1a39364ec8e9113868161d7e6
amyhue <128926023+amyhue@users.noreply.github.com>
2023-04-23 13:52:54 -0700

    1:52PM 4/23/2023

--- 23Apr2023/Transit.cpp
+++ 23Apr2023/Transit.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <string>;
 #include <vector>
 
 int main() {
 	// get start time, end time, # of transit routes
 	int s, t, n;
-	std::vector<int> d, b, c, w;
+	std::vector<int> d, b, c;
+	int time; // how much time he has to get to class
+	int tot_time; // how much time it takes to get to class
+	int ti = 0; // travel time after bus_i "completes" each route (walk time, bus route time, and waiting time)
+
 	std::cin >> s >> t >> n;
 
 	// get walk travel times
@@ -29,30 +34,32 @@ int main() {
 	}
 
 	// how long it takes to wait for each bus
-	int ti = 0;
 
 	for (int i = 0; i < n; i++) {
 		int wi, ci, x;
-		
-		ti += d.at(i) + b.at(i) + w.at(i);
+
 		ci = c.at(i);
-		
+		ti += d.at(i);
 		if (ti % ci == 0) wi = 0;
 		else {
 			x = int(ti / ci) + 1;
+			/*std::cout << "Debug: " << ti << ", " << ci << std::endl;
+			std::cout << "Debug: " << x;*/
 			wi = ci*x - ti;
 		}
-		w.push_back(wi);
 
+		/*std::cout << std::endl;
+		std::cout << d.at(i) << ", " << b.at(i) << ", " << wi <<std::endl;*/
+
+		ti += b.at(i) + wi;
 	}
 
+	time = t - s;
+	tot_time = ti + d.at(d.size() - 1);
+	/*std::cout << time << std::endl;
+	std::cout << tot_time<<std::endl;*/
 
-	
-
-	int time = t - s; // how much time he has to get to class
-	int tot_time = 0; // how much time it takes to get to class
-
-
-
+	if (time >= tot_time) std::cout << "yes";
+	else std::cout << "no";
 
 }
